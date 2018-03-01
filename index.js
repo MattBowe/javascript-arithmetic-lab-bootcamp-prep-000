24c57b1dd937e02cd84923be0ad2de41dd9c7745
bowematthew13@gmail.com Bowe <bowematthew13@gmail.com>
2018-03-01 17:23:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a,b) {
+  return (a+b);
+}
+function subtract(a,b) {
+  return (a-b);
+}
+function multiply(a,b) {
+  return (a*b);
+}
+function divide(a,b) {
+  return (a/b);
+}
+function inc(a) {
+  return (a+1);
+}
+function dec(a) {
+  return (a-1);
+}
+function makeInt(n) {
+  var parse = parseInt(n);
+  parseInt('a');
+   return parse;
+   function makeInt(n, base) {
+     
+    parseInt('0x2328', 10);
+    return parse;
+    
+   }
+    parseInt('sldkjflksjf');
+    if(isNaN(parse)) {return true}
+   
+}
+function preserveDecimal(n) {
+  var float = parseFloat(n);
+  parseFloat('2.222');
+  return float;
+  function preserveDecimal(n) {
+  parseFloat('sldkjflksjf');
+  if(isNaN(float)) {return true}
+  }
+}
+
